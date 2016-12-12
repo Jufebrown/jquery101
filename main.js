dbea48e84eb1d71cd0c9865e02fd19ee42cad866
Jufebrown <jufebrown@gmail.com>
2016-12-12 11:17:46 -0600

    more jquery examples

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-console.log('hello')
+console.log('hello');
 $("h1").html("Hello Friends");
+
+var foods = ["apple","banana","carrot","danish","egg"];
+
+for (var i = 0; i < foods.length; i++) {
+  $("ul").append("<li>" + foods[i] + "</li>");
+}
+
+$("ul").wrap(`<div class="foods"></div>`);
+// $("ul").parent().toggleClass("foods");

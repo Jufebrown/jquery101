0a825f8bd728061d95408dc817ddd1e5a87afe64
Jufebrown <jufebrown@gmail.com>
2016-12-12 12:22:39 -0600

    more jquery examples

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ for (var i = 0; i < foods.length; i++) {
 
 $("ul").wrap(`<div class="foods"></div>`);
 // $("ul").parent().toggleClass("foods");
+
+$("input:first").val("new value");
+
+$("input:last").click(function() {
+  console.log($("input:first").val());
+  $("input:first").val("");
+});
+
+$.getJSON("https://randomuser.me/api/?nat=us", function(data){
+
+  $("h1:first").html("Hello " + data.results[0].name.first.charAt(0).toUpperCase())
+})
